9e831783285af256675a358373d6ca916c0221cf
Jim Hall <jim.hall@solarmedia.com>
2017-04-26 22:39:22 -0400

    Add location-list and location-info views

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,16 +1,16 @@
 /* GET 'Home' page */
 const homeList = function(req, res) {
-    res.render('index', { title: 'Home' });
+    res.render('locations-list', { title: 'Home' });
 };
 
 /* GET 'Location Info' page */
 const locationInfo = function(req, res) {
-    res.render('index', { title: 'Location Info' });
+    res.render('location-info', { title: 'Location Info' });
 };
 
 /* GET "Add Review" page */
 const addReview = function(req, res) {
-    res.render('index', { title: 'Add Review' });
+    res.render('location-review-form', { title: 'Add Review' });
 };
 
 module.exports = {
