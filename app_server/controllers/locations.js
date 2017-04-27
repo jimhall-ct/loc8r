4acc7249a8366b5991cbbdc1570377513822e0b9
Jim Hall <jim.hall@solarmedia.com>
2017-04-27 10:24:10 -0400

    Create mixin functions to simplify views

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,16 +1,42 @@
 /* GET 'Home' page */
-const homeList = function(req, res) {
-    res.render('locations-list', { title: 'Home' });
+const homeList = function (req, res) {
+    res.render('locations-list', {
+        title: 'Loc8r - find a place to work with wifi',
+        pageHeader: {
+            title: 'Loc8r',
+            strapline: 'Find places to work with wifi near you!'
+        },
+        sidebar: "Looking for wifi and a seat? Loc8r helps you find places to work when out and about. Perhaps with coffee, cake or a pint? Let Loc8r help you find the place you're looking for.",
+        locations: [{
+            name: 'Starcups',
+            address: '125 High Street, Reading, RG6 1PS',
+            rating: 3,
+            facilities: ['Hot Drinks', 'Food', 'Premium WiFi'],
+            distance: '100m'
+        }, {
+            name: 'Cafe Hero',
+            address: '125 High Street, Reading, RG7 2PS',
+            rating: 4,
+            facilities: ['Hot Drinks', 'Food', 'Premium WiFi', 'Clean Bathrooms'],
+            distance: '200m'
+        }, {
+            name: 'Burger Queen',
+            address: '125 High Street, Reading, RG8 3PS',
+            rating: 2,
+            facilities: ['Food', 'Premium WiFi'],
+            distance: '250m'
+        }]
+    });
 };
 
 /* GET 'Location Info' page */
-const locationInfo = function(req, res) {
-    res.render('location-info', { title: 'Location Info' });
+const locationInfo = function (req, res) {
+    res.render('location-info', {title: 'Location Info'});
 };
 
 /* GET "Add Review" page */
-const addReview = function(req, res) {
-    res.render('location-review-form', { title: 'Add Review' });
+const addReview = function (req, res) {
+    res.render('location-review-form', {title: 'Add Review'});
 };
 
 module.exports = {
